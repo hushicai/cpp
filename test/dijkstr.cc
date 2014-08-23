0d0e08f3901258bedf5b8ea654de03336ab44071
hushicai <bluthcy@gmail.com>
2014-08-23 15:33:46 +0800

    update dijkstra

--- test/dijkstr.cc
+++ test/dijkstr.cc
@@ -2,6 +2,10 @@
 #include <vector>
 using namespace std;
 
+// ## 参考文章
+// * http://baike.baidu.com/view/1712262.htm?fromtitle=Dtra%E7%AE%97%E6%B3%95&fromid=215612&type=syn#2
+// * http://blog.csdn.net/jnu_simba/article/details/8866705
+
 // # 算法步骤
 // * S: 最短路径集合，一个顶点属于集合S当且仅当从源到该顶点的最短路径长度已知。
 // * G: 所有顶点
@@ -11,7 +15,6 @@ using namespace std;
 // * 贪心: 每次循环取出d中距离源点最小的顶点，将其添加到S中
 // * 迭代：设u是S中最短路径顶点集合的末端，更新d中的相对最短路径
 // * 结束：S = G
-// 
 // ps: 所谓相对最短路径，就是指相对于u而言
 
 // 最简单的实现方式，时间复杂度O(V^2)
@@ -21,7 +24,6 @@ void Dijkstra()
     const int n = 5;
     // 源点
     const int start = 0;
-    // http://blog.csdn.net/jnu_simba/article/details/8866705
     // 邻接矩阵
     //定义有向图
     int map[][n] = {                     
@@ -31,12 +33,14 @@ void Dijkstra()
         {9, INT_MAX, INT_MAX, 0, 8},
         {INT_MAX, INT_MAX, 5, INT_MAX, 0}
     };
-    // 源点到各节点的距离
+    // 源点到各顶点的相对最短路径
     int d[n];
     // 已找到的最短路径集合
     int S[n];
-    vector<bool> isInS(5, false);
+    // 标志是否在S中
+    vector<bool> isInS(n, false);
     
+    // 根据有向图初始化
     for(int i = 0; i < n; ++i)
     {
         d[i] = map[start][i];
@@ -82,13 +86,13 @@ void Dijkstra()
     }
 
     cout << "最短路径顶点集合：";
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < n; ++i)
     {
         cout << S[i] << " ";
     }
     cout << endl;
 
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < n; ++i)
     {
         cout << "源点" << start << "到顶点" << i << "的最短距离为：" << d[i] << endl;
     }
